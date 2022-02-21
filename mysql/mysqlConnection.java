129c152e7d267191b9994f273d783157afb9737a
AjithJohnson <99463897+AjithJohnson@users.noreply.github.com>
2022-02-21 09:24:57 +0530

    jdbc connection

---
+++ mysql/mysqlConnection.java
@@ -0,0 +1,28 @@
+package trainingjava;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MysqlConnector {
+
+	public static void main(String[] args) {
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/ajithdb","root","root");
+			Statement s= c.createStatement();
+			ResultSet r= s.executeQuery("select * from EMP");
+			
+			while(r.next())
+			{
+				System.out.println(r.getInt(1)+r.getString(2));
+			}
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
