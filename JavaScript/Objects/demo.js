9d478867ec579b048f9cc58f6f86056fc38c4d85
Ajith Johnson                                                                                         (211442) <99463897+AjithJohnson@users.noreply.github.com>
2022-03-11 08:26:31 +0530

    Add files via upload

---
+++ JavaScript/Objects/demo.js
@@ -0,0 +1,30 @@
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: "email@gmail.com",
+    location: 'berlin',
+    bolgs: ['why mac & cheese rules'];
+
+
+// console.log(user);
+
+login: function() {
+    console.log('the user logged in');
+},
+logout: function() {
+    console.log('the user logged out');
+},
+
+logBlogs: function() {
+    this.bolgs
+
+}
+
+};
+
+user.logBlogs();
+
+
+// this keyword
+
