8122423fcc0bb3acc12313483e63d2b2b2fc8485
Ajith Johnson                                                                                         (211442) <99463897+AjithJohnson@users.noreply.github.com>
2022-03-21 22:22:53 +0530

    Add files via upload

---
+++ TypeScript/Abstract Class/Rectangle.ts
@@ -0,0 +1,44 @@
+
+
+import { Shape } from './Shape';
+
+export class Rectangle extends Shape {
+    calculateArea(): number {
+        return this.width *  this._length;
+    }
+
+    
+  
+
+constructor(thenX: number, thenY: number,
+    private _width: number,   private _length: number ) {
+
+    super(thenX, thenY);
+
+
+}
+
+
+    public get width(): number {
+        return this._width;
+    }
+    public set width(value: number) {
+        this._width = value;
+    }
+
+    
+    public get length(): number {
+        return this._length;
+    }
+    public set length(value: number) {
+        this._length = value;
+    }
+
+    getInfo(): string {
+        return super.getInfo() + ` , width=${this._width}, length=${this._length}`;
+    }
+
+
+
+
+}
