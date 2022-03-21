740f31c85eb88cfed38c34e71e57d623398ca737
Ajith Johnson                                                                                         (211442) <99463897+AjithJohnson@users.noreply.github.com>
2022-03-21 22:24:05 +0530

    Add files via upload

---
+++ TypeScript/Interfaces/CricketCoach.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CricketCoach = void 0;
+class CricketCoach {
+    getDailyWorkout() {
+        return "practice spin balling";
+    }
+}
+exports.CricketCoach = CricketCoach;
