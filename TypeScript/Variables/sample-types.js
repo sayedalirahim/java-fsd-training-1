9e5bff6f28d71c9698621cca25b331c9fb797b12
Ajith Johnson                                                                                         (211442) <99463897+AjithJohnson@users.noreply.github.com>
2022-03-21 22:20:15 +0530

    Add files via upload

---
+++ TypeScript/Variables/sample-types.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 88.3;
+var firstName = "Anup";
+var lastName = "kumar";
+console.log(found);
+console.log("the grade is " + grade);
+console.log("hi");
+console.log("hi " + firstName + " " + lastName);
---
+++ TypeScript/Variables/sample-types.ts
@@ -0,0 +1,20 @@
+
+
+let found: boolean = true;
+let grade: number = 88.3;
+
+let firstName: string ="Anup";
+let lastName: string = "kumar";
+
+
+// let
+
+console.log(found);
+console.log("the grade is " +grade);
+console.log("hi");
+console.log("hi " + firstName + " " +lastName);
+
+
+
+
+
